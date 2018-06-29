32fc37e85c7f350a828eaa4305d4f59e25a912dd
Volker Süß <versus167@gmail.com>
2018-06-29 12:33:25 +0200

    init

---
+++ zfsbackup/zfsbackup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+Created on 28.06.2018
+
+@author: Volker Süß
+'''
+
+APPNAME='zfsbackup'
+VERSION='1 - 2018-06-28'
+
+class zfs_fs(object):
+    '''
+    Alles rund um das Filesystem direkt
+    '''
+    def __init__(self,fs,connection):
+        '''
+        Welches FS und worübererreichen wir das
+        '''
+        pass
+
+
+class zfs_back(object):
+    '''
+    
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+if __name__ == '__main__':
+    a = zfs_back()
