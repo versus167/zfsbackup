24ca1acc48714cdb742bea4666f5214dd55ca055
Volker Süß <volker.suess@gmail.com>
2021-09-01 16:10:58 +0200

    zfsbackup_receiver.py

---
+++ zfsbackup/zfsbackup_receiver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+
+Created on 01.09.2021
+
+@author: Volker Süß
+
+Agiert als Receiver für den zfs receive befehl - damit nicht zu viele Rechte an zfs gegeben werden, muss das so laufen
+'''
