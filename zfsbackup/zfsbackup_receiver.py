517058b0bd779a4a1a05303e9fac5f36cd5b234e
Volker Süß <volker.suess@gmail.com>
2021-09-01 16:11:59 +0200

    re

--- zfsbackup/zfsbackup_receiver.py
+++ zfsbackup/zfsbackup_receiver.py
@@ -8,3 +8,11 @@ Created on 01.09.2021
 
 Agiert als Receiver für den zfs receive befehl - damit nicht zu viele Rechte an zfs gegeben werden, muss das so laufen
 '''
+
+
+import re
+
+re.fullmatch(r'^zfs receive -vs [^- ][^ ]*',"zfs receive -vs F-Fsdf/madklö")
+
+pass
+
